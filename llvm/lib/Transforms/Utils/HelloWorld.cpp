0d60df37f3e2878e932e3da630185cb1b855deb1
Min <manypotatoes1@gmail.com>
2026-03-02 14:07:55 +0800

    Implement instruction and basic block counting
    learn pass
    Added instruction and basic block counting to HelloWorldPass.

--- llvm/lib/Transforms/Utils/HelloWorld.cpp
+++ llvm/lib/Transforms/Utils/HelloWorld.cpp
@@ -7,12 +7,31 @@
 //===----------------------------------------------------------------------===//
 
 #include "llvm/Transforms/Utils/HelloWorld.h"
+#include "llvm/IR/BasicBlock.h"
 #include "llvm/IR/Function.h"
+#include "llvm/IR/Instruction.h"
 
 using namespace llvm;
 
 PreservedAnalyses HelloWorldPass::run(Function &F,
                                       FunctionAnalysisManager &AM) {
+  size_t InstCount = 0;
   errs() << F.getName() << "\n";
+  size_t BBIndex = 0;
+  for (const BasicBlock &BB : F) {
+    errs() << "bb#" << BBIndex;
+    if (BB.hasName())
+      errs() << " (" << BB.getName() << ")";
+    errs() << ":\n";
+
+    size_t InstIndexInBB = 0;
+    for (const Instruction &I : BB) {
+      ++InstCount;
+      errs() << "  [" << InstIndexInBB << "] " << I << "\n";
+      ++InstIndexInBB;
+    }
+    ++BBIndex;
+  }
+  errs() << "instruction-count: " << InstCount << "\n";
   return PreservedAnalyses::all();
 }
